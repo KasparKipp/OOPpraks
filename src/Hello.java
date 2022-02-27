44b7979ee084a05328f447485a8277f6c385f9dc
Kaspar <kaspar.kipp@gmail.com>
2022-02-27 13:44:35 +0200

    lõpetuseks ka stashitud muudatused up

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("a friend was here but stayed too long,\n" +
                 "a conflict resolved makes a friend lifelong");
+        //TODO: Teen muudatusi mida stashida ja hiljem kasutada
     }
 }
